f3d1a55edbd7fcc7ad788174ace61ef45404d9fc
Erika Hernandez <erika.hernandez@correo.unimet.edu.ve>
2024-03-05 18:28:45 -0400

    fix: Packages and names

--- JavaApplication2/src/Main/Classes/Main.java
+++ JavaApplication2/src/Main/Classes/Main.java
@@ -2,13 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication2;
+package Main.Classes;
+
+import GUI.Classes.Home;
 
 /**
  *
  * @author user
  */
-public class JavaApplication2 {
+public class Main {
 
     /**
      * @param args the command line arguments
